e38e5b954b492cc0cded00e9682b477bd60a4835
massdo <dorian_massoulier@hotmail.com>
2020-10-15 20:52:39 +0200

    FT with unittest

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,27 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
+class NewVisitorTest(unittest.TestCase):
+    
+    def setUp(self):
+        self.browser = webdriver.Firefox()
 
-assert 'Django' in browser.title
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        # Scénario "Robert découvre le site":
+        # Etant donné Robert un visiteur qui a entendu parler de notre site
+        # Quand il saisit l'url de notre site via son navigateur
+        self.browser.get('http://localhost:8000')
+        # Alors il peut lire "To-Do" dans l'onglet
+        self.assertIn('To-Do', self.browser.title)
+        # ON FAIT ECHOUER LE TEST VOLONTAIREMENT CAR INCOMPLET
+        self.fail('FInish the test!')
+        # Et on lui propose de saisir une note de texte.
+
+
+        # [suite des scénarios pour plus tard ...]
+
+if __name__ == '__main__':
+    unittest.main()
