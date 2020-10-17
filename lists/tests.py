0dd8db648f91d3238297129590e9685f4f52abf7
massdo <dorian_massoulier@hotmail.com>
2020-10-17 19:15:27 +0200

    First UT, about url root path mapped to empty home_page view

--- lists/tests.py
+++ lists/tests.py
@@ -1,6 +1,9 @@
+from django.urls import resolve
 from django.test import TestCase
+from lists.views import home_page
 
-class ExampleTest(TestCase):
+class HomePageTest(TestCase):
 
-    def test_addition_in_parallel_universe(self):
-        self.assertEqual(1+1, 3)
+    def test_root_url_resolve_to_home_page_view(self):
+        my_view = resolve('/')
+        self.assertEqual(my_view.func, home_page)
