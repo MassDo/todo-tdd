a026d659d5b1bed587b814c64ffdb9c6d4c032c5
massdo <dorian_massoulier@hotmail.com>
2020-10-19 19:50:56 +0200

    Refactor home_view to use template add lists app to the project's setting INSTALLED_APPS Refactor the unit tests

--- lists/tests.py
+++ lists/tests.py
@@ -1,18 +1,8 @@
-from django.http import HttpRequest
-from django.urls import resolve
 from django.test import TestCase
-from lists.views import home_page
 
 class HomePageTest(TestCase):
 
-    def test_root_url_resolve_to_home_page_view(self):
-        my_view = resolve('/')
-        self.assertEqual(my_view.func, home_page)
 
-    def test_to_do_in_title_home_page(self):
-        request = HttpRequest()
-        response = home_page(request)
-        html = response.content.decode('utf8')
-        self.assertTrue(html.startswith('<html>'))
-        self.assertIn('<title>To-Do</title>', html)
-        self.assertTrue(html.endswith('</html>'))
+    def test_uses_home_template(self):
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'home.html')
